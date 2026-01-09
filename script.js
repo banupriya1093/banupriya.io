5c0493f9fda5c3cc4c6f060d39dc2fd9f5ceed11
banupriya1093 <banupriya.s1093@gmail.com>
2026-01-09 21:59:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-//console.log(names.length);
-for (var i=0; i < names.length ; i++) {
-	//console.log(names[i]);
-	//console.log(names[i].indexOf("J"));
-  if ((names[i].indexOf("J")) || (names[i].indexOf("j")) == 0) {
-    console.log("Hello "+ names[i]);
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
   } else {
-    console.log("Good Bye "+ names[i]);
+    helloSpeaker.speak(names[i]);
   }
 }
